2e8597bc8b10d7b7f30b61de3654e76b569bd337
abcussi <cussi.angel.benjamin@gmail.com>
2020-04-28 07:58:08 -0400

    auto fix lintern and change validatiion on user.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,12 @@
 class UsersController < ApplicationController
-    @user = User.new(user_params)
- 
-    def new
-        @user = User.new
-    end
-  
-    def create
-    #@user = User.new(username: params[:username], email: params[:email], password: params[:password])
+  @user = User.new(user_params)
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
     @user = User.new(user_params)
     if @user.save
       redirect_to new_user_path
@@ -31,7 +31,7 @@ class UsersController < ApplicationController
 
   private
 
-    def user_params
-        params.require(:user).permit(:username,:email,:password)
-    end
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end

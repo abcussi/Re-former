b1c760ef144503fa3a9f923421f4f75f7ebbf0d7
abcussi <cussi.angel.benjamin@gmail.com>
2020-04-28 07:13:37 -0400

    add Html form create file add token

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,21 @@
 class UsersController < ApplicationController
+    @user = User.new(user_params)
+ 
     def new
     end
   
     def create
+        
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+    def user_params
+        params.require(:user).permit(:username,:email,:password)
     end
 end
